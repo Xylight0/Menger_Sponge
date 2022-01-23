9c3d2a31e4b327b8ea00b25d80b46938acbd6756
ngrepp <ngrepp@stud.hs-offenburg.de>
2022-01-23 20:02:29 +0100

    finished exercise

--- sketch.js
+++ sketch.js
@@ -1,29 +1,66 @@
-let a = 0;
-
-let box1 = {
-  "v1": null,
-  "v2": null,
-  "size": 200,
-}
+let rotation = 0;
+let boxList = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  box1.v1 = createVector(40, 50);
-  box1.v2 = createVector(40, 50);
+  boxList.push({
+    vector: createVector(0, 0, 0),
+    size: 200,
+  });
+  frameRate(144);
 }
 
 function draw() {
   background(080);
-  rotateX(a);
-  rotateY(a);
+  rotateX(rotation);
+  rotateY(rotation);
+  lights();
 
   noFill();
   stroke(333, 333);
 
-  show(box1);
-  a += 0.01;
+  boxList.forEach((boxE) => {
+    show(boxE);
+  });
+  rotation += 0.01;
 }
 
-function show(boxI){
+function mousePressed() {
+  let newBoxList = [];
+  boxList.forEach((boxI) => {
+    generate(boxI).forEach((e) => newBoxList.push(e));
+  });
+  boxList = newBoxList;
+}
+
+function show(boxI) {
+  push();
+  fill(255);
+  noStroke();
+  translate(boxI.vector.x, boxI.vector.y, boxI.vector.z);
   box(boxI.size);
+  pop();
+}
+
+function generate(boxI) {
+  let newBoxList = [];
+  for (let x = -1; x < 2; x++) {
+    for (let y = -1; y < 2; y++) {
+      for (let z = -1; z < 2; z++) {
+        let sum = abs(x) + abs(y) + abs(z);
+        if (sum > 1) {
+          let newSize = boxI.size / 3;
+          newBoxList.push({
+            vector: createVector(
+              boxI.vector.x + x * newSize,
+              boxI.vector.y + y * newSize,
+              boxI.vector.z + z * newSize
+            ),
+            size: newSize,
+          });
+        }
+      }
+    }
+  }
+  return newBoxList;
 }
